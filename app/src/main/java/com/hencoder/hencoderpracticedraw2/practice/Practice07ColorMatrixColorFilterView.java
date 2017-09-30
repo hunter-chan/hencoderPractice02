2a4bd85f465903853a06713bdf914ab31b0eaf33
chenqx <chan.qx@qq.com>
2017-09-30 12:28:56 +0800

    finish the majority of the practice2 from hencoder

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
@@ -0,0 +1,60 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
+
+public class Practice07ColorMatrixColorFilterView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+
+    public Practice07ColorMatrixColorFilterView(Context context) {
+        super(context);
+    }
+
+    public Practice07ColorMatrixColorFilterView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice07ColorMatrixColorFilterView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+
+        // 使用 setColorFilter() 设置一个 ColorMatrixColorFilter
+        // 用 ColorMatrixColorFilter.setSaturation() 把饱和度去掉
+
+//        ColorFilter colorFilter = new ColorMatrixColorFilter();
+        //需使用Matrix
+        float[] GREY_SCALE = new float[] {
+                0.33F, 0.59F, 0.11F, 0, 0,
+                0.33F, 0.59F, 0.11F, 0, 0,
+                0.33F, 0.59F, 0.11F, 0, 0,
+                0, 0, 0, 1, 0,
+        };
+        ColorMatrix colorMatrix = new ColorMatrix(GREY_SCALE);
+        ColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        paint.setColorFilter(colorFilter);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+    }
+}
